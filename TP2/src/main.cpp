1c2b41f21b3b583dc7e2c41385e941629cfdc8e7
MathGaron <mathieugaron91@gmail.com>
2015-02-06 18:54:29 -0500

    add chrono

--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include "PngImage.h"
+#include "Chrono.hpp"
 
 int getArgument(int argc, char **argv);
 void usage(char* inName);
 
 int main(int argc, char **argv) {
+    Chrono chrono(false);
+    std::cout << "The resolution on this machine is: " << chrono.getRes() << " sec" << std::endl;
+    chrono.resume();
+    double startTime = chrono.get();
+
     PngImage exampleImg("../Image/exemple.png");
     PngImage newImg(*exampleImg.getData(), exampleImg.getWidth(), exampleImg.getHeight());
 
     newImg.writeToDisk("../Image/output.png");
+
+    std::cout << "Total Time: " << chrono.get() - startTime << " sec" << std::endl;
     return 0;
 }
 
