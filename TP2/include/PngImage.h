5fcc90ce49832d831e2232641221b61473eb0df1
MathGaron <mathieugaron91@gmail.com>
2015-02-06 19:59:54 -0500

    made PngImage iterable and [] accessible

--- TP2/include/PngImage.h
+++ TP2/include/PngImage.h
@@ -15,20 +15,39 @@ public:
 
     void writeToDisk(const char* filename);
 
+    typedef std::vector<unsigned char>::iterator iterator;
+    typedef std::vector<unsigned char>::const_iterator const_iterator;
+
+    iterator begin();
+    const_iterator begin() const;
+    iterator end();
+    const_iterator end() const;
+
+    unsigned char operator[](int i) const;
+
+    unsigned char & operator[](int i);
+
 private:
+
     unsigned int width;
     unsigned int height;
     std::vector<unsigned char>  *imageData;
 };
 
-inline unsigned int PngImage::getHeight() const{
-    return this->height;
-}
+inline unsigned char &PngImage::operator[](int i) {return this->imageData->operator[](i);}
 
-inline unsigned int PngImage::getWidth() const{
-    return this->width;
-}
+inline unsigned char PngImage::operator[](int i) const {return this->imageData->operator[](i);}
 
-inline std::vector<unsigned char>* PngImage::getData() const{
-    return this->imageData;
-}
+inline iterator PngImage::begin() { return this->imageData->begin(); }
+
+inline const_iterator PngImage::begin() const { return this->imageData->begin(); }
+
+inline iterator PngImage::end() { return this->imageData->end(); }
+
+inline const_iterator PngImage::end() const { return this->imageData->end(); }
+
+inline unsigned int PngImage::getHeight() const{return this->height;}
+
+inline unsigned int PngImage::getWidth() const{return this->width;}
+
+inline std::vector<unsigned char>* PngImage::getData() const{return this->imageData;}
