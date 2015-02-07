d3ab9c4bcd3fb30486db175b815db9214b32c3ff
MathGaron <mathieugaron91@gmail.com>
2015-02-07 10:05:20 -0500

    add filter object

---
+++ TP2/include/Filter.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include "Tokenizer.hpp"
+#include <vector>
+
+class Filter {
+public:
+    Filter(std::string path);
+    ~Filter();
+
+    double operator[](int i) const;
+    double & operator[](int i);
+
+private:
+    int loadSize();
+    void loadFilterData();
+
+    int filterSize;
+    double *data;
+    PACC::Tokenizer *token;
+
+};
+
+inline double &Filter::operator[](int i) {return this->data[i];}
+inline double Filter::operator[](int i) const {return this->data[i];}
+
--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include "PngImage.h"
+#include "Filter.h"
 #include "Chrono.hpp"
+#include "omp.h"
 
 int getArgument(int argc, char **argv);
 void usage(char* inName);
 
 int main(int argc, char **argv) {
     Chrono chrono(false);
-    std::cout << "The resolution on this machine is: " << chrono.getRes() << " sec" << std::endl;
     chrono.resume();
-    double startTime = chrono.get();
 
     PngImage exampleImg("../Image/exemple.png");
-    PngImage newImg(*exampleImg.getData(), exampleImg.getWidth(), exampleImg.getHeight());
-
-    newImg.writeToDisk("../Image/output.png");
-
+    Filter filter("../filters/noyau_flou");
+    double startTime = chrono.get();
+    int tid;
+    #pragma omp parallel private(tid)
+    {
+        tid = omp_get_thread_num();
+        printf("Thread : %d\n",tid);
+        if(tid == 0){
+            printf("Number of threads : %d\n",omp_get_num_threads());
+        }
+    }
     std::cout << "Total Time: " << chrono.get() - startTime << " sec" << std::endl;
+
     return 0;
 }
 
