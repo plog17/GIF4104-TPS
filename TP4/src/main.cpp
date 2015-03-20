164ad2ad94a551cb8fb35a0a76d019f617102d50
MathGaron <mathieugaron91@gmail.com>
2015-03-20 18:00:13 -0400

    fix opencl lib link

--- TP4/src/main.cpp
+++ TP4/src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <stdexcept>
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.hpp>
 
 int main(int argc, char** argv) {
-    std::cout << "Main!" << std::endl;
+    cl_int status; // return value for most OpenCL functions
+    cl_uint numPlatforms = 0;
+
+    //get all platforms (drivers)
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+    if(all_platforms.size()==0){
+        std::cout<<" No platforms found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Platform default_platform=all_platforms[0];
+    std::cout << "Using platform: "<<default_platform.getInfo<CL_PLATFORM_NAME>()<<"\n";
 }
